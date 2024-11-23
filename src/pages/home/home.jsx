a656c8f7f68874dfdf088ddf73d95b0d40324dad
morquecho-s <mssm011009@gmail.com>
2024-11-23 00:31:01 -0800

    update:A links from Home Section and the typingEffects

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,31 +2,29 @@ import { useState, useEffect } from 'react';
 import Dotino from "../../components/dotino";
 import SocialMediaLinks from "../../components/socialmedialinks";
 import "./style.css"
+import {UseTypingEffect} from "../../hooks/typingEffect"
+import { Paragraph } from '../../components/paragraph';
 
-const UseTypingEffect = (text, delay,sep=1) => {
-    const [typedText, setTypedText] = useState("");
-    const [index, setIndex] = useState(0);
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTypedText(text.substring(0, index));
-            setIndex(index + sep);
-        }, delay);
-
-        return () => clearInterval(timer);
-    }, [index, text, delay]);
-
-    return typedText;
-};
 
 const Home = ({textSite})=>{
     
     const descriptionText = textSite.homepage.description
-    
-    const typedContentCreator = UseTypingEffect(
+    const [typedContentCreator,setContentCreator] = UseTypingEffect(
         textSite.homepage['simple-description'].split("\n")[0],
         50, 1);
-    const typedPickleEnthusiast = UseTypingEffect(textSite.homepage['simple-description'].split("\n")[1], 100, 1);
+    const [typedProgrammer,setProgrammer] = UseTypingEffect(textSite.homepage['simple-description'].split("\n")[1], 100, 1);
+    const [typedPickleEnthusiast,setPickleEnthusiast] = UseTypingEffect(textSite.homepage['simple-description'].split("\n")[2], 100, 1);
+    const handleSmoothScroll = (e, targetId) => {
+        e.preventDefault();
+    
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      };
     return(
         <section id="home-section">
             <Dotino></Dotino>
@@ -38,33 +36,38 @@ const Home = ({textSite})=>{
                 </div>
                 Dot <br /> Dager
             </h1>
-            <h2>
+            <h2 style={{textAlign:'right'}}>
                 {typedContentCreator}
+                <br />
+                {typedProgrammer}
                 <br />
                 {typedPickleEnthusiast}
             </h2>
             <SocialMediaLinks/>
-            <p className="description-of-dot-dager">
+            <Paragraph className="description-of-dot-dager">
                 {descriptionText}
-            </p>
+            </Paragraph>
+            
             <div className="media-buttons">
                 <img src="/public/images/cat.png" alt="" className="cat"/>
-                <button>
+                <a  className="button" href='#contact-me' onClick={(e)=>handleSmoothScroll(e,"contact-me")}>
                     <span>
                         {
                             textSite.buttons.contanctme
                         }
                     </span>
                     <img src="/public/icons/right row.png" />
-                </button>
-                <button className="blue">
-                    <span>
-                        {
-                            textSite.buttons['about me']
-                        }
-                    </span>
-                    <img src="/public/icons/right row.png" />
-                </button>
+                </a>
+                
+                    <a href="#aboutMe" className="button blue" onClick={(e)=>handleSmoothScroll(e,"aboutMe")}>
+                        <span>
+                            {
+                                textSite.buttons['about me']
+                            }
+                        </span>
+                        <img src="/public/icons/right row.png" />
+                    </a>
+                
             </div>
 
         </section>
