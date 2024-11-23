587610eb3f23b4292ceceb41ed7474f2258ba7ab
morquecho-s <mssm011009@gmail.com>
2024-11-23 00:34:21 -0800

    update: implement the sections on the App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import Home from "./pages/home/home"
 import "./app.css"
 import englishSite from "./englishsite.json"
 import spanishSite from "./spanishsite.json"
-
+import AboutMe from './pages/aboutme/aboutme';
+import ContactMe from './pages/contactme/contacme';
+import { SwitchLanguage } from './components/switch-language';
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const url = this.href;
+      scrollSuaveYRedirigir(url);
+  });
+});
 function App() {
   const [site_on_english, setSiteOnEnglish] = useState(true);
   const [textSite, setTextSite] = useState(site_on_english?englishSite:spanishSite);
@@ -14,11 +23,16 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setSiteOnEnglish(!site_on_english)} id='btn-change-language'>
-        {site_on_english ? 'Switch to Spanish' : 'Switch to English'}
-      </button>
+      <SwitchLanguage 
+        id='global-switch-language'
+        onChange={(language)=>{
+          setSiteOnEnglish(language === 'en')
+        }}
+        ></SwitchLanguage>
       <Home textSite={textSite}/>
-
+      <AboutMe textSite={textSite}/>
+      <div id='extra-information'/>
+      <ContactMe textSite={textSite}/>
     </>
   )
 }
